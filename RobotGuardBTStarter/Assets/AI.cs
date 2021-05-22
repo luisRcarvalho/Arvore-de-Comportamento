a505f50f580c0fc79e94e6c46b3749eb0c731b3d
luisRcarvalho <luisricardocarvalhogomes@hotmail.com.br>
2021-05-21 22:33:24 -0300

    Attack, Look Around e Patrol

--- RobotGuardBTStarter/Assets/AI.cs
+++ RobotGuardBTStarter/Assets/AI.cs
@@ -25,43 +25,112 @@ public class AI : MonoBehaviour
     {
         agent = this.GetComponent<NavMeshAgent>(); //associando o componente navmeshagent a variavel agent
         agent.stoppingDistance = shotRange - 5; //for a little buffer
-        InvokeRepeating("UpdateHealth",5,0.5f);//invoca o metodo que recupera a vida depois de 5segs e fica recuperando a cada 0.5segs
+        InvokeRepeating("UpdateHealth", 5, 0.5f);//invoca o metodo que recupera a vida depois de 5segs e fica recuperando a cada 0.5segs
     }
 
     void Update()
     {
         Vector3 healthBarPos = Camera.main.WorldToScreenPoint(this.transform.position); // faz a barra de vida acompanhar a camera
         healthBar.value = (int)health; //referencia o valor da barra de vida a variavel de vida do player
-        healthBar.transform.position = healthBarPos + new Vector3(0,60,0); //define a posição da barra de vida
+        healthBar.transform.position = healthBarPos + new Vector3(0, 60, 0); //define a posição da barra de vida
     }
 
     void UpdateHealth()
     {
         if (health < 100)//se a vida ficar menor que 100, ele recupera 1 de vida
-        health ++;
+            health++;
     }
 
     void OnCollisionEnter(Collision col)
     {
-        if(col.gameObject.tag == "bullet")//se colidir com uma bala, ele perde 10 de vida
+        if (col.gameObject.tag == "bullet")//se colidir com uma bala, ele perde 10 de vida
         {
             health -= 10;
         }
     }
-    [Task] 
-    public void PickRandomDestination() 
-    { 
+    [Task]
+    public void PickRandomDestination()
+    {
         Vector3 dest = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100));//variavel de vector "dest" que recebe um X e Z aleatorios
         agent.SetDestination(dest);// realiza a movimentação para o dest gerado
         Task.current.Succeed();//define a task com sucesso
     }
-    [Task] 
+    [Task]
     public void MoveToDestination()
     {
-        if (Task.isInspected)Task.current.debugInfo = string.Format("t={0:0.00}", Time.time); //quando a task é pega, mostra o tempo que esta sendo executada
-        if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending) { 
+        if (Task.isInspected) Task.current.debugInfo = string.Format("t={0:0.00}", Time.time); //quando a task é pega, mostra o tempo que esta sendo executada
+        if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+        {
             Task.current.Succeed(); //se a distancia percorrida for menor que a distancia pra ele parar,considera a task bem sucedida
         }
     }
-}
 
+    [Task]
+    public void TargetPlayer()
+    {
+        target = player.transform.position; //define o alvo sendo o player(ja pegando o position dele)
+        Task.current.Succeed();
+    }
+    [Task]
+    public bool Fire()
+    {
+        GameObject bullet = GameObject.Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);//instancia os tiros
+        bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward * 2000);//adiciona força aos tiros para que eles vão para frente
+        return true;
+    }
+
+    [Task]
+    public void LookAtTarget()
+    {
+        Vector3 direction = target - this.transform.position; // define a direção sendo ela o alvo(player) menos a sua posição
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);//realiza uma rotação
+        if (Task.isInspected)
+        {
+            Task.current.debugInfo = string.Format("angle={0}", Vector3.Angle(this.transform.forward, direction));
+        }
+
+        if (Vector3.Angle(this.transform.forward, direction) < 5.0f)
+        {
+            Task.current.Succeed();
+        }
+    }
+    [Task] public void PickDestination(int x, int z)
+    {
+        Vector3 dest = new Vector3(x, 0, z);//define o destino, sendo o ponto X e Z definidos pelo script
+        agent.SetDestination(dest);//faz a movimentação para o destino
+        Task.current.Succeed(); 
+    }
+    [Task]
+    bool SeePlayer()
+    {
+        Vector3 distance = player.transform.position - this.transform.position; //define a direção sendo ela o alvo(player) menos a sua posição 
+        RaycastHit hit;//cria um raycast
+        bool seeWall = false; //bool para detectar parede
+        Debug.DrawRay(this.transform.position, distance, Color.red);//cria um debug para poder visualizar o raycast
+        if(Physics.Raycast(this.transform.position, distance, out hit))
+        {
+            if (hit.collider.gameObject.tag == "wall")
+            {
+                seeWall = true; //se o raycast colidir com um objeto com tag wall, ele declara true na booleana
+            }
+        }
+
+        if (Task.isInspected)
+        {
+            Task.current.debugInfo = string.Format("wall={0}", seeWall);
+        } 
+        if(distance.magnitude < visibleRange && !seeWall)
+        {
+            return true;
+        }
+        else return false;
+    }
+
+    [Task] 
+    bool Turn(float angle)
+    { 
+        var p = this.transform.position + Quaternion.AngleAxis(angle, Vector3.up) * this.transform.forward;
+        target = p;
+        return true; 
+    }
+}
